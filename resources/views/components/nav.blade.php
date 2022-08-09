573caaeaae3a896be339ee1b83d22057a0134b82
samircfc1996 <samirmemmedov804@gmail.com>
2022-08-09 17:36:39 +0400

    MainMenu Add

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,10 @@
+<nav class="collapsible-nav" id="collapsible-nav">
+
+    @foreach($items as $menu_item)
+
+        <a href="{{ $menu_item->route }}" class="nav-link    {{ request()->routeIs($menu_item->route)?"active":"" }}">{{ $menu_item->title }}</a>
+
+    @endforeach
+
+    
+</nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,13 +15,7 @@
 <header>
     <button class="btn btn-white btn-share ml-auto mr-3 ml-md-0 mr-md-auto"><img src="assets/images/share.svg" alt="share" class="btn-img">
         SHARE</button>
-    <nav class="collapsible-nav" id="collapsible-nav">
-        <a href="/" class="nav-link    {{ request()->routeIs('home')?"active":"" }}">HOME</a>
-        <a href="/resume" class="nav-link    {{ request()->routeIs('resume')?"active":"" }}">RESUME</a>
-        <a href="/portfolio" class="nav-link {{ request()->routeIs('portfolio')?"active":"" }}">PORTFOLIO</a>
-        <a href="/blog" class="nav-link     {{ request()->routeIs('blog')?"active":"" }}">BLOG</a>
-        <a href="/contact" class="nav-link   {{ request()->routeIs('contact')?"active":"" }}">CONTACT</a>
-    </nav>
+    {{ menu('mainMenu', 'components.nav') }}
     <button class="btn btn-menu-toggle btn-white rounded-circle" data-toggle="collapsible-nav"
             data-target="collapsible-nav"><img src="assets/images/hamburger.svg" alt="hamburger"></button>
 </header>
